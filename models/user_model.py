8582fc049bf513834c8c5d0aa8be0ba927d1b60a
okanfer <43836810+okanfer@users.noreply.github.com>
2025-05-15 20:28:45 +0300

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -72,13 +72,7 @@ class User(db.Model):
             
     @staticmethod
     def get_user(username): # SQLi Injection 
-        user_query = f"SELECT * FROM users WHERE username = '{username}'"
-        query = db.session.execute(text(user_query))
-        ret = query.fetchone()
-        if ret:
-            fin_query = '{"username": "%s", "email": "%s"}' % (ret[1], ret[3])
-        else:
-            fin_query = None
+        fin_query = User.query.filter_by(username=username).first()
         return fin_query
 
     @staticmethod
