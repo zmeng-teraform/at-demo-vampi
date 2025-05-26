893e3651f2cb5dac830fa4b08d4458d7d99179db
okanfer <43836810+okanfer@users.noreply.github.com>
2025-05-26 11:29:28 +0300

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -66,14 +66,9 @@ class User(db.Model):
         return [User.json_debug(user) for user in User.query.all()]
 
     def get_user(username): # SQLi Injection 
-        user_query = f"SELECT * FROM users WHERE username = '{username}'"
-        query = db.session.execute(text(user_query))
-        ret = query.fetchone()
-        if ret:
-            fin_query = '{"username": "%s", "email": "%s"}' % (ret[1], ret[3])
-        else:
-            fin_query = None
+        fin_query = User.query.filter_by(username=username).first()
         return fin_query
+
     @staticmethod
     def delete_user(username):
         done = User.query.filter_by(username=username).delete()
