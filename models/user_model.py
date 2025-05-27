9af53a77fb45124ff090dc424dd0f444ffbb1cc6
earkush <150027879+earkush@users.noreply.github.com>
2025-05-27 09:22:59 +0300

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -66,7 +66,7 @@ class User(db.Model):
         return [User.json_debug(user) for user in User.query.all()]
 
     def get_user(username): # SQLi Injection 
-        user_query = "SELECT * FROM users WHERE username = '{username}'"
+        user_query = f"SELECT * FROM users WHERE username = '{username}'"
         query = db.session.execute(text(user_query))
         ret = query.fetchone()
         if ret:
