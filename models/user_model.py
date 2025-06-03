3cc7dadbb3c5603e9fb2e4d3b455fd3945707c3f
earkush <150027879+earkush@users.noreply.github.com>
2025-06-03 10:02:04 +0300

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -64,6 +64,10 @@ class User(db.Model):
     @staticmethod
     def get_all_users_debug():
 	return [User.json_debug(user) for user in User.query.all()]
+
+    def get_user(username): # SQLi Injection 
+        fin_query = User.query.filter_by(username=username).first()
+        return fin_query
 	    
     @staticmethod
     def delete_user(username):
