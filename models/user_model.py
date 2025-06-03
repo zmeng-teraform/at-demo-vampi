1ea26829e01667e63f9380dfe58f3ff96855dc6f
earkush <150027879+earkush@users.noreply.github.com>
2025-06-03 11:23:46 +0300

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -65,9 +65,8 @@ class User(db.Model):
     def get_all_users_debug():
         return [User.json_debug(user) for user in User.query.all()]
 
-    @staticmethod
     def get_user(username):
-	    fin_query = User.query.filter_by(username=username).first()
+	fin_query = User.query.filter_by(username=username).first()
         return fin_query
 
     @staticmethod
