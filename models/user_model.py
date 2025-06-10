048de12e73c2defafb39ea3555a6ccbb817c2bba
jchai-akam <jchai@akamai.com>
2025-06-10 19:40:51 +1000

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -65,19 +65,9 @@ class User(db.Model):
     def get_all_users_debug():
         return [User.json_debug(user) for user in User.query.all()]
 
-    def get_user(username):
-        if vuln:  # SQLi Injection
-            user_query = f"SELECT * FROM users WHERE username = '{username}'"
-            query = db.session.execute(text(user_query))
-            ret = query.fetchone()
-            if ret:
-                fin_query = '{"username": "%s", "email": "%s"}' % (ret[1], ret[3])
-            else:
-                fin_query = None
-        else:
-            fin_query = User.query.filter_by(username=username).first()
+    def get_user(username): # SQLi Injection 
+        fin_query = User.query.filter_by(username=username).first()
         return fin_query
-
 
     @staticmethod
     def register_user(username, password, email, admin=False):
