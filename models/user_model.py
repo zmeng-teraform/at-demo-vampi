5291f06f9f394bb43ab6933487c529bd6334d85a
okanfer <43836810+okanfer@users.noreply.github.com>
2025-09-02 11:42:13 +0300

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -70,6 +70,18 @@ class User(db.Model):
         db.session.commit()
         return done
 
+    def get_user(username):
+        if vuln:  # SQLi Injection
+            user_query = f"SELECT * FROM users WHERE username = '{username}'"
+            query = db.session.execute(text(user_query))
+            ret = query.fetchone()
+            if ret:
+                fin_query = '{"username": "%s", "email": "%s"}' % (ret[1], ret[3])
+            else:
+                fin_query = None
+        else:
+            fin_query = User.query.filter_by(username=username).first()
+        return fin_query
         
     @staticmethod
     def register_user(username, password, email, admin=False):
